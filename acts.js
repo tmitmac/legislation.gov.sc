bb300a5fbae7ce85521499ab6a79066252e114ff
tmitmac <trentmckenzie09@outlook.com>
2026-07-27 18:17:09 +1200

    Update acts.js

--- acts.js
+++ acts.js
@@ -1,5 +1,6 @@
 const acts = [
   /* 2020s */
+  { number: "2026 No 03", name: "Ministry of Agriculture, Fisheries, and Natural Resources Establishment Act 2026", file: "viewer.html?act=2000-2099/2026/ministry-of-agriculture-fisheries-and-natural-resources-establishment-act-2026.txt", year: 2026, type: "act", inForce: true, bill: false, agency: "Ministry of Agriculture, Fisheries, and Natural Resources"},  
   { number: "189—1", name: "Westland (Tierras del oeste) Empowerment Amendment Bill", file: "viewer.html?act=2000-2099/2026/westland-tierras-del-oeste-empowerment-amendment-bill.txt", year: 2026, type: "bill", inForce: false, bill: true, agency: "Ministry of Justice & Corrections"},
   { number: "188—1", name: "Employment Holidays Bill", file: "viewer.html?act=2000-2099/2026/employment-holidays-bill.txt", year: 2026, type: "bill", inForce: false, bill: true, agency: "Ministry of Labour & Employment"},
   { number: "187—1", name: "Appropriations (June Estimates) Bill", file: "viewer.html?act=2000-2099/2026/appropriations-(june-estimates)-bill-2026.txt", year: 2026, type: "bill", inForce: false, bill: true, agency: "Ministry of Finance"},
@@ -9,6 +10,7 @@ const acts = [
   { number: "2025 No 43", name: "Defence Expansion Act 2025", file: "viewer.html?act=2000-2099/2025/defence-expansion-act-2025.txt", year: 2025, type: "act", inForce: true, bill: false, agency: "Ministry of Defence"},
   { number: "2025 No 42", name: "Federal Revenue and Tax Reform Act 2025", file: "viewer.html?act=2000-2099/2025/federal-revenue-and-tax-reform-act-2025-version-2.txt", year: 2025, type: "act", inForce: true, bill: false, agency: "Ministry of Finance"},
   /* 2010s */
+  { number: "", name: "", file: "viewer.html?act=2000-2099/", year: 2015, type: "act", inForce: true, bill: false, agency: ""},
   { number: "2015 No 53", name: "Education and Training Act 2015", file: "viewer.html?act=2000-2099/2015/education-and-training-act-2015.txt", year: 2015, type: "act", inForce: true, bill: false, agency: "Ministry of Education"},
   { number: "2013 No 36", name: "Electoral (Social Media Advertising) Amendment Act 2013", file: "viewer.html?act=2000-2099/2013/electoral-(social-media-advertising)-amendment-act-2013.txt", year: 2013, type: "act", inForce: true, bill: false, agency: "Ministry of Justice & Corrections"},
   /* 2000s */
@@ -20,6 +22,7 @@ const acts = [
   { number: "1996 No 109", name: "Electoral Amendment Act 1996", file: "viewer.html?act=1900-1999/1996/electoral-amendment-act-1996.txt", year: 1996, type: "act", inForce: true, bill: false, agency: "Ministry of Justice & Corrections"},
   { number: "1996 No 108", name: "Westland (Tierras del oeste) Empowerment Act 1996", file: "viewer.html?act=1900-1999/1996/westland-tierras-del-oeste-empowerment-act-1996.txt", year: 1996, type: "act", inForce: true, bill: false, agency: "Ministry of Justice & Corrections"},
   { number: "1992 No 107", name: "Electoral Amendment Act 1992", file: "viewer.html?act=1900-1999/1992/electoral-amendment-act-1992.txt", year: 1992, type: "act", inForce: true, bill: false, agency: "Ministry of Justice & Corrections"}, 
+  { number: "1990 No 59", name: "Public Information Act 1990", file: "viewer.html?act=1900-1999/1990/public-information-act-1990.txt", year: 1990, type: "act", inForce: true, bill: false, agency: "Ministry of Justice & Corrections"}, 
   /* 1980s */
   { number: "1988 No 72", name: "Public Holidays Act 1988", file: "viewer.html?act=1900-1999/1988/public-holidays-act-1988.txt", year: 1988, type: "act", inForce: true, bill: false, agency: "Ministry of Labour & Employment"}, 
   { number: "1986 No 33", name: "Electoral Act 1986", file: "viewer.html?act=1900-1999/1986/electoral-act-1986.txt", year: 1986, type: "act", inForce: true, bill: false, agency: "Ministry of Justice & Corrections"},
@@ -31,6 +34,7 @@ const acts = [
   { number: "1963 No 08", name: "Crimes Act 1964", file: "viewer.html?act=1900-1999/1964/crimes-act-1964.txt", year: 1964, type: "act", inForce: true, bill: false, agency: "Ministry of Justice & Corrections"}, 
   /* 1950s */
   { number: "1953 No 47", name: "Electoral Act 1953", file: "viewer.html?act=1900-1999/1953/electoral-act-1953.txt", year: 1953, type: "act", inForce: false, bill: false, agency: "Ministry of Justice & Corrections"},
+  { number: "1950 No 78", name: "Constitution Act 1950", file: "viewer.html?act=1900-1999/1950/constitution-act-1950.txt", year: 1950, type: "act", inForce: true, bill: false, agency: "Ministry of Justice & Corrections"},
   { number: "1950 No 02", name: "Wildlife Act 1950", file: "viewer.html?act=1900-1999/1950/wildlife-act-1950.txt", year: 1950, type: "act", inForce: true, bill: false, agency: "Wildlife Conservation Agency"},
   /* 1940s */ 
   { number: "1949 No 08", name: "Education Act 1949", file: "viewer.html?act=1900-1999/1949/education-act-1949.txt", year: 1949, type: "act", inForce: false, bill: false, agency: "Ministry of Education"},
